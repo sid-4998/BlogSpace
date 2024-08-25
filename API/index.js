6f7dbb412dea923dc9aa57bf3942e2d9dd3c4525
Your Name <siddhantjha34@gmail.com>
2024-08-25 11:04:18 +0530

    Connected to Database

--- API/index.js
+++ API/index.js
@@ -1,4 +1,13 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose'; 
+process.loadEnvFile(".env");
+mongoose.connect(process.env.MONGO)
+.then(()=> {
+    console.log("Database is connected!");
+})
+.catch((err)=> {
+    console.log(err);
+})
 const app = express();
 
 
