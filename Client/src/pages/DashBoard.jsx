6343df2463b6e8c00e3ccaadc28906d876f5af61
Your Name <siddhantjha34@gmail.com>
2024-09-26 17:42:52 +0530

    Show users to the Admin Dashboard.

--- Client/src/pages/DashBoard.jsx
+++ Client/src/pages/DashBoard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import DashBoardSideBar from '../components/DashBoardSideBar';
 import DashBoardProfile from '../components/DashBoardProfile';
 import DashPost from '../components/DashPost';
+import DashUsers from '../components/DashUsers';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -24,6 +25,8 @@ export default function Dashboard() {
       {tab === 'profile' && <DashBoardProfile />}
       {/* posts... */}
       {tab === 'posts' && <DashPost />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
     </div>
   );
 }
