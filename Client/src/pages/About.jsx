8e7239bcac0659f8514200f18cb2fb75a15cb9a1
Your Name <siddhantjha34@gmail.com>
2024-09-28 17:55:44 +0530

    Complete About Page.

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About my Blog!
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome! This blog was created by me
+              as a personal project to share my thoughts and ideas with the
+              world. I am a passionate developer who loves to write about
+              technology, coding, and everything in between.
+            </p>
+            <p>
+              On this blog, you will find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. I am always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+            <p>
+              I encourage you to leave comments on our posts and engage with
+              other readers. You can like comments made by other people and reply to
+              them as well. I believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
