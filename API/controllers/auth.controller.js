bb389d12e8de94ed1def3da3c7299c6f8d655f0a
Your Name <siddhantjha34@gmail.com>
2024-10-21 23:07:57 +0530

    Updated another file.

--- API/controllers/auth.controller.js
+++ API/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
-process.loadEnvFile(".env");
+import 'dotenv/config';
 export const SignUp = async (req, res, next) => {
   const { username, email, password } = req.body;
 
